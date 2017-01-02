b5a3326cbf357a541dd414cb2c21c4149c0672e6
枯叶_听海 <5490584810@163.com>
2017-01-02 22:43:56 +0800

    init project

---
+++ src/main/java/code/showme/HelloWorld.java
@@ -0,0 +1,10 @@
+package code.showme;
+
+/**
+ * Created by jeremie on 2017/1/2.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
